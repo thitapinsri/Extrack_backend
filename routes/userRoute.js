d66679720078acdeb882bcb1ff3a18b99175d0dd
Thita Pinsri <thitapinsri@gmail.com>
2022-08-21 21:50:39 +0700

    initialize project

---
+++ routes/userRoute.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const userRoute = express.Router();
+const userController = require('../controllers/userController')
+
+const authSession = require('../middleware/authSession')
+
+userRoute.use(authSession);
+// path = user/activities
+const activitiesRoute = require('../routes/activitiesRoute')
+userRoute.use('/activities',activitiesRoute);
+
+
+// userRoute.get('/', userController.allUsers);
+userRoute.get('/me', userController.getUserById);
+userRoute.patch('/me', userController.editUser);
+
+
+
+
+module.exports = userRoute;
