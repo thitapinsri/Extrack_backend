95da098c361f847d9c93a9fc2d690aa4d3756d5c
Thita Pinsri <thitapinsri@gmail.com>
2022-08-24 20:10:36 +0700

    add new client domain

--- api/index.js
+++ api/index.js
@@ -24,7 +24,12 @@ const cors = require("cors");
 app.use(
   cors({
     credentials: true,
-    origin: ['https://extract-frontend-mu.vercel.app', 'http://localhost:5173', 'https://extract-frontend-two.vercel.app'],
+    origin: [
+      'https://extract-frontend-mu.vercel.app',
+      'http://localhost:5173',
+      'https://extract-frontend-two.vercel.app',
+      'https://extracts.vercel.app'
+    ],
   })
 );
 
